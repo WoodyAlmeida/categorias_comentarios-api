79ce37d341ff6597e7dfd566c8b410b488d7a004
eduardo.almeida <eduardo.almeida@tvtem.com>
2020-10-28 00:07:06 -0300

    + index

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+const app_1 = require("./app");
+let app = new app_1.default();
+app.start();
+//CommandService.start()
+//# sourceMappingURL=index.js.map
